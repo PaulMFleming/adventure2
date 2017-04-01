c63caebd805a2702746aadab609651cc48d4ad83
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-04-01 10:22:48 +0100

    Added some more locations, fixed directions

--- adventure.py
+++ adventure.py
@@ -3,8 +3,8 @@ from data import locations
 directions = {
     'west': (-1, 0),
     'east': (1, 0),
-    'north': (0, -1),
-    'south': (0, 1),
+    'south': (0, -1),
+    'north': (0, 1),
 }
 
 position = (0, 0)
@@ -22,4 +22,9 @@ while True:
             valid_directions[k] = possible_position
 
     direction = raw_input('which direction do you want to go?\n')
-    position = valid_directions[direction]
+
+    new_position = valid_directions.get(direction)
+    if new_position:
+        position = new_position
+    else:
+         print "sorry that's not a valid direction"
--- data.py
+++ data.py
@@ -3,4 +3,9 @@ locations = {
     (0, 1): 'lake',
     (1, 0): 'park',
     (1, 1): 'market',
+    (-1, 0): 'forest',
+    (-1, 1): 'field',
+    (-1, -1): 'supermarket',
+    (1, -1): 'school',
+    (0, -1): 'garden',
 }
